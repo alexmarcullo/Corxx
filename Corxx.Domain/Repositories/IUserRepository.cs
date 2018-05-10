b314fe23d7b9c8bb84e98b6e18ab6b2e566c42c8
Alex Marçullo Teixeira Gonçalves <alex.marcullo@sysmi.com.br>
2018-05-10 18:58:43 -0300

    Inicializando a infra

---
+++ Corxx.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Corxx.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corxx.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<ICollection<User>> GetAllAsync();
+
+        Task<User> GetByIdAsync(Guid id);
+
+        Task<User> GetByEmailAsync(Guid id);
+
+        Task SaveAsync(User entity);
+
+        Task UpdateAsync(User entity);
+
+        Task DeleteAsync(User entity);
+
+    }
+}
---
+++ Corxx.Domain/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Corxx.Domain.Services
+{
+    public interface IEmailService
+    {
+        Task SendAsync(string from, string to, string subject, string body);
+    }
+}
